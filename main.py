ab7fcbf49c9cc85b624821b6d727f951000ac4d7
Maxime-Desclaux <129112688+Maxime-Desclaux@users.noreply.github.com>
2024-04-03 15:25:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,13 +15,15 @@ def main():
 
         if choice == "1":
             print_quote(random_quote(quotes))
-        elif choice == "2":
-            view_quotes(quotes)
+        elif choice == "2"
+            count = int(input("Enter the number of quotes to display: "))
+            display_quotes(quotes, count)
         elif choice == "3":
             print("Good bye...")
             break
         else:
             print("Invalid input")
+            
 
 if __name__ == "__main__":
     main()
